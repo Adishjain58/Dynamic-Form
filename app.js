860c577c8c437f438a6500f5cb09fdbbba28ec04
Adish Jain <adishjain589@gmail.com>
2022-03-26 18:48:56 +0530

    fixed deploy issue

--- app.js
+++ app.js
@@ -7,19 +7,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("Client/form/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "Client", "form", "build", "index.html")
-    );
-  });
-}
-
-const port = 3001 || process.env.port;
-
 app.post("/formConfig", (req, res) => {
   fs.writeFileSync("./config.json", JSON.stringify(req.body));
   res.status(200).json({ success: true });
@@ -30,6 +17,22 @@ app.get("/getFormConfig", (req, res) => {
   res.status(200).json({ data });
 });
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "/Client/form/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Client/form/build/index.html"));
+  });
+}
+
+// app.use(express.static(path.join(__dirname, "/Client/form/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/Client/form/build/index.html"));
+// });
+// console.log(path.join(__dirname, "/Client/form/build/index.html"));
+
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
